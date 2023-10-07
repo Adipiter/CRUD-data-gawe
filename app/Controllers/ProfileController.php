0f13ad5fa1d90ba0e261a072aaf2867ff133f739
adimiuprix <miuprix@gmail.com>
2023-10-07 14:01:33 +0700

    melengkapi kometar

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -35,11 +35,14 @@ class ProfileController extends Controller
             }
     
             // Generate nama unik untuk file foto profil
-            $fileName = $fotoProfile->getRandomName();
-    
+            // $fileName = $fotoProfile->getRandomName();
+
+            // Tidak mengubah nama filenya
+            $fileName = $fotoProfile->getName();
+
             // Pindahkan file foto profil ke direktori yang ditentukan
             $fotoProfile->move($uploadPath, $fileName);
-    
+
             // Instansiasi Model
             $ProfileModel = new ProfileModel();
 
@@ -49,18 +52,19 @@ class ProfileController extends Controller
             // Cek apakah profil sudah ada untuk ID yang ditentukan
             $profileAda = $ProfileModel->where('id', $modifikasi)->first();
 
+            // Kondisi jika profile ada
             if ($profileAda) {
                 // Ambil nama file yang akan dihapus
-                $oldFileName = $profileAda['filename'];
-                
+                $namaLama = $profileAda['filename'];
+
                 // Hapus file lama sebelum menyimpan yang baru
-                unlink(ROOTPATH . 'public/logo/' . $oldFileName);
+                unlink(ROOTPATH . 'public/logo/' . $namaLama);
 
                 // Jika profil sudah ada, perbarui data profilnya
                 $ProfileModel->update($profileAda['id'], ['filename' => $fileName]);
             } else {
                 // Jika tidak ada profil, buat profil baru untuk ID yang ditentukan
-                $ProfileModel->insert(['id' => 1, 'filename' => $fileName]);
+                $ProfileModel->insert(['filename' => $fileName]);
             }
 
             return redirect()->to('profile')->with('success', 'Foto profil berhasil diunggah');
